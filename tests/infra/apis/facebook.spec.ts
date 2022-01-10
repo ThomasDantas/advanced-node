b9aca27d8773b8f72925a41b9ab868571b140363
ThomasDantas <thomas@paipe.co>
2022-01-10 10:18:02 -0300

    aula 23

---
+++ tests/infra/apis/facebook.spec.ts
@@ -0,0 +1,33 @@
+import { HttpGetClient } from '@/infra/http'
+import { FacebookApi } from '@/infra/apis'
+
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('FacebookApi', () => {
+  let clientId: string
+  let clientSecret: string
+  let sut: FacebookApi
+  let httpClient: MockProxy<HttpGetClient>
+
+  beforeAll(() => {
+    clientId = 'any_client_id'
+    clientSecret = 'any_client_secret'
+    httpClient = mock()
+  })
+  beforeEach(() => {
+    sut = new FacebookApi(httpClient, clientId, clientSecret)
+  })
+
+  it('should get app token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/oauth/access_token',
+      params: {
+        client_id: clientId,
+        client_secret: clientSecret,
+        grant_type: 'client_credentials'
+      }
+    })
+  })
+})
