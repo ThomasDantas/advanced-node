2022896b684ca8c601052dbf64350e71f2b70649
ThomasDantas <thomas@paipe.co>
2022-01-13 10:45:06 -0300

    aula 41

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,5 +2,6 @@ export const env = {
   facebookApi: {
     clientId: process.env.FB_CLIENT_ID ?? '701947017857121',
     clientSecret: process.env.FB_CLIENT_SECRET ?? '1ac506460ea01035e8fdf018a9ae9d49'
-  }
+  },
+  port: process.env.PORT ?? 8080
 }
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,1 +1,7 @@
+import './config/main-alias'
+
+import { app, env } from '@/main/config'
+
 import 'reflect-metadata'
+
+app.listen(env.port, () => console.log(`Server Running at http://localhost:${env.port}`))
