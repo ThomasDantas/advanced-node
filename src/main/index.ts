37107924a8c200a1e3b365c142d6647119bc2832
ThomasDantas <thomas@paipe.co>
2022-01-14 14:31:04 -0300

    aula 47

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,11 @@
 import './config/main-alias'
 
 import { app, env } from '@/main/config'
+import { config } from '@/infra/postgres/helpers'
 
 import 'reflect-metadata'
+import { createConnection } from 'typeorm'
 
-app.listen(env.port, () => console.log(`Server Running at http://localhost:${env.port}`))
+createConnection(config)
+  .then(() => app.listen(env.port, () => console.log(`Server Running at http://localhost:${env.port}`)))
+  .catch(console.error)
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -14,7 +14,7 @@ describe('Facebook Api Integration Tests', () => {
     clientId = env.facebookApi.clientId
     clientSecret = env.facebookApi.clientSecret
     sut = new FacebookApi(axiosClient, clientId, clientSecret)
-    token = 'EAAJZBasamiGEBAAUFY0EVxTyI2fhq9RryOPQCZBEPz7oHUmWOibUJLws043L8B3u6FOr84yJcxQ7RFF77dOXGdntYdthyEgxoZBUVsP6icb1jEXRwvDnAICb1yhBb1Jma977pLYKO0oj94eJN0HtcDDdG02xwXR4YSvT7macD5JqlQc2u7GBt6RvDlC4o6yoxeHwce2rQZDZD'
+    token = env.facebookApi.token
   })
 
   it('should return a Facebook User if token is valid', async () => {
