37fbd76ae738e5194754bbcad3b91821c5c15e12
ThomasDantas <thomas@paipe.co>
2022-01-18 14:45:46 -0300

    aula 58

--- src/application/middlewares/authentication.ts
+++ src/application/middlewares/authentication.ts
@@ -2,13 +2,12 @@ import { forbidden, ok } from '@/application/helpers/http'
 import { HttpResponse } from '@/application/helpers'
 import { Middleware } from '@/application/middlewares'
 import { RequiredStringValidator } from '@/application/validation'
-import { Authorize } from '@/domain/use-cases'
 
 type HttpRequest = {
   authorization: string
 }
 type Model = Error | { userId: string }
-
+type Authorize = (params: { token: string }) => Promise<string>
 export class AuthenticationMiddleware implements Middleware {
   constructor (private readonly authorize: Authorize) {}
 
