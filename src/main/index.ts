9f373a3e8305f4d9d680c45875dcc72fe96255e9
ThomasDantas <thomas@paipe.co>
2022-01-26 17:20:33 -0300

    aula 90

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,13 +1,10 @@
 import './config/main-alias'
 
 import 'reflect-metadata'
-import { createConnection, getConnectionOptions } from 'typeorm'
+import { createConnection } from 'typeorm'
 
-getConnectionOptions()
-  .then(async options => {
-    const root = process.env.TS_NODE_DEV === undefined ? 'dist' : 'src'
-    const entities = [`${root}/infra/repos/postgres/entities/index.{js,ts}`]
-    await createConnection({ ...options, entities })
+createConnection()
+  .then(async () => {
     const { app, env } = await import('@/main/config')
     app.listen(env.port, () => console.log(`Server Running at http://localhost:${env.port}`))
   })
